fe2acd686fa293ba251008242d6eabb1af5a5526
Gabriel Barreto <gabrielb.defigueiredo@gmail.com>
2023-10-23 05:45:39 -0300

    animação inicial

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-function navBar() {
-  let menuMobile = document.querySelector('.header--navbar');
+const  navBar = () => {
+  const menuMobile = document.querySelector('.header--navbar');
   if (menuMobile.classList.contains('open')) {
       menuMobile.classList.remove('open');
       document.querySelector('.icon').src = "src/img/bx-menu.svg";
@@ -16,3 +16,22 @@ for (const link of links) {
     })
 }
 }
+
+const animaElements = document.querySelectorAll('.js-scroll');
+const windowMetade = window.innerHeight * 0.62;
+console.log(windowMetade)
+const animate = () => {
+    animaElements.forEach((anima) => {
+        const animaTop = anima.getBoundingClientRect().top;
+        const isAnimaElementsVisible = (animaTop - windowMetade) < 0 ;
+        if(isAnimaElementsVisible){
+        anima.classList.add('ativo');
+        }else{
+            anima.classList.remove('ativo');  
+        }
+    });
+}
+
+animate();
+
+window.addEventListener('scroll', animate)
